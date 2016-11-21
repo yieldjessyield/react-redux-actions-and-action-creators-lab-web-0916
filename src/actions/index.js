ba15f8097c193568cab17923a7213a30f6bb9774
Jess <jess.harrelson0707@gmail.com>
2016-11-21 14:21:42 -0500

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,43 @@
 // define your actions here!
+
+// ex:
+// export function incrementCount(){
+//   return {
+//     type: 'INCREMENT_COUNT'
+//   }
+// }
+
+// function createTweet(tweet) {
+//   return {
+//     type: 'CREATE_TWEET',
+//     tweet: tweet
+//   }
+// }
+
+export function addMovie(movie){
+  return{
+    type: 'ADD_MOVIE',
+    movie: movie
+  }
+}
+
+export function deleteMovie(movie){
+  return {
+    type: 'DELETE_MOVIE',
+    movie: movie
+  }
+}
+
+export function updateUsername(name){
+  return {
+    type: 'UPDATE_USERNAME',
+    username: name
+  }
+}
+
+export function resetUsername(){
+  return {
+    type: 'RESET_USERNAME',
+    username: 'default'
+  }
+}
